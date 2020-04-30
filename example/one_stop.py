822578c2bb5c6c3dd82b5a766ad90fed2e549391
Kevin Li ☠️ <kanwli@microsoft.com>
2020-04-30 16:35:07 +0800

    UPDATE

--- example/one_stop.py
+++ example/one_stop.py
@@ -4,12 +4,14 @@ from pokemon_bot import PokemonBot
 COLLECTING_NEEDED = True
 HATCHING_NEEDED = True
 
-N_COLLECTING = 360
+N_COLLECTING = 14 * 30 - 6
 LAPS = 3
 
-N_HATCHING = 360
+N_HATCHING = 18 * 30
 CYCLE = 20
 CURRENT_COLUMN = 1
+
+PLACEHOLDER_MOVE_PACE = -11
 
 SLEEP_AFTER_COMPLETION = True
 
@@ -19,10 +21,12 @@ if __name__ == "__main__":
         if COLLECTING_NEEDED:
             bot.collect_eggs(N_COLLECTING, LAPS, not HATCHING_NEEDED and SLEEP_AFTER_COMPLETION)
             ctrl.pause(1)
+
         if COLLECTING_NEEDED and HATCHING_NEEDED:
             bot.fly_to_daycare_at_bridgefield()
             ctrl.pause(1)
-            bot.move_placeholder_pokemon(-10)
+            bot.move_placeholder_pokemon(PLACEHOLDER_MOVE_PACE)
             ctrl.pause(1)
+
         if HATCHING_NEEDED:
             bot.hatch_eggs(N_HATCHING, CYCLE, CURRENT_COLUMN, SLEEP_AFTER_COMPLETION)
