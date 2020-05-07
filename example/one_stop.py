d5c5b7491afda9386d5c27a2b7aa263dcb2b3d6b
Kevin Li ☠️ <kanwli@microsoft.com>
2020-05-07 09:15:07 +0800

    update

--- example/one_stop.py
+++ example/one_stop.py
@@ -1,15 +1,15 @@
 from NXController import Controller
 from pokemon_bot import PokemonBot
 
-COLLECTING_NEEDED = True
+COLLECTING_NEEDED = False
 HATCHING_NEEDED = True
 
-N_COLLECTING = 14 * 30 - 6
+N_COLLECTING = 5 * 30
 LAPS = 3
 
-N_HATCHING = 18 * 30
+N_HATCHING = 13 * 30
 CYCLE = 20
-CURRENT_COLUMN = 1
+CURRENT_COLUMN = 2
 
 PLACEHOLDER_MOVE_PACE = -11
 
@@ -23,10 +23,12 @@ if __name__ == "__main__":
             ctrl.pause(1)
 
         if COLLECTING_NEEDED and HATCHING_NEEDED:
+            ctrl.release()
             bot.fly_to_daycare_at_bridgefield()
             ctrl.pause(1)
             bot.move_placeholder_pokemon(PLACEHOLDER_MOVE_PACE)
             ctrl.pause(1)
 
         if HATCHING_NEEDED:
+            ctrl.release()
             bot.hatch_eggs(N_HATCHING, CYCLE, CURRENT_COLUMN, SLEEP_AFTER_COMPLETION)
