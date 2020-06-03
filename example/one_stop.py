342a049716977f5ea5099f07df2ac9fc75f06051
python大鹌鹑 <likanwen@icloud.com>
2020-06-04 07:20:58 +0800

    update

--- example/one_stop.py
+++ example/one_stop.py
@@ -4,23 +4,25 @@ from pokemon_bot import PokemonBot
 COLLECTING_NEEDED = True
 HATCHING_NEEDED = True
 
-N_COLLECTING = 18 * 30
+N_COLLECTING = 9 * 30 - 9
 LAPS = 3
 
-N_HATCHING = 11 * 30 - 10
+N_HATCHING = 18 * 30
 CYCLE = 20
-CURRENT_COLUMN = 3
+CURRENT_COLUMN = 1
 
 PLACEHOLDER_MOVE_PACE = -11
 
 SLEEP_AFTER_COMPLETION = True
-NO_EGG_LEFT_MODE = False
+NO_EGG_LEFT_MODE = True
 
 if __name__ == "__main__":
     with Controller() as ctrl:
         bot = PokemonBot(ctrl)
         if COLLECTING_NEEDED:
-            bot.collect_eggs(N_COLLECTING, LAPS, not HATCHING_NEEDED and SLEEP_AFTER_COMPLETION)
+            bot.collect_eggs(
+                N_COLLECTING, LAPS, not HATCHING_NEEDED and SLEEP_AFTER_COMPLETION
+            )
             ctrl.pause(1)
 
         if COLLECTING_NEEDED and HATCHING_NEEDED:
@@ -32,4 +34,10 @@ if __name__ == "__main__":
 
         if HATCHING_NEEDED:
             ctrl.release()
-            bot.hatch_eggs(N_HATCHING, CYCLE, CURRENT_COLUMN, SLEEP_AFTER_COMPLETION, NO_EGG_LEFT_MODE)
+            bot.hatch_eggs(
+                N_HATCHING,
+                CYCLE,
+                CURRENT_COLUMN,
+                SLEEP_AFTER_COMPLETION,
+                NO_EGG_LEFT_MODE,
+            )
